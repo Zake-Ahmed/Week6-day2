aff9bf5fe171edd76ece6d5c4bc858ef35399373
zake3 <zake3012@gmail.com>
2022-06-07 11:32:32 +0000

    add py file

---
+++ Task1.py
@@ -0,0 +1,7 @@
+name = input("Please input your first name")
+lastName = input("Please input your last name")
+age = int(input("Please input your age"))
+height = float(input("Please input your height in meters"))
+
+print("Your height is ", height,"m")
+print("Your age in 10 years will be ", age+10)
